f58257fb85ff94c7d29f06381eb4c335ed9fc517
ernestpazera <ernestpazera@gmail.com>
2020-02-01 09:38:50 -0600

    background pellet timer and mostly complete

--- Snek/main/board.go
+++ Snek/main/board.go
@@ -28,3 +28,78 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "bgm"
+  component: "/assets/audio/bgm.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "turn"
+  component: "/assets/audio/turn.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "chomp"
+  component: "/assets/audio/chomp.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "death"
+  component: "/assets/audio/death.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "penalty"
+  component: "/assets/audio/penalty.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
