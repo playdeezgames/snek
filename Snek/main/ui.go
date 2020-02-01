50b03d9a1f60c4546f281f91564f0f1d5cf5601a
ernestpazera <ernestpazera@gmail.com>
2020-02-01 17:13:46 -0600

    donish

---
+++ Snek/main/ui.go
@@ -0,0 +1,75 @@
+components {
+  id: "ui"
+  component: "/main/ui.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "gameOver"
+  type: "sprite"
+  data: "tile_set: \"/main/background.atlas\"\n"
+  "default_animation: \"gameover\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 320.0
+    y: 240.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "title"
+  type: "sprite"
+  data: "tile_set: \"/main/background.atlas\"\n"
+  "default_animation: \"snek\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 320.0
+    y: 240.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "zToStart"
+  type: "sprite"
+  data: "tile_set: \"/main/background.atlas\"\n"
+  "default_animation: \"ztostart\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 320.0
+    y: 32.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
